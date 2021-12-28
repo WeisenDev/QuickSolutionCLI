5f41df55d5feadb7253e86b89aeb58b39b2199cd
Michael Kovalenko <weisendevelopment@gmail.com>
2021-12-28 23:06:39 +0300

    Добавлены методы и реализовано сложение чисел
    Добавлены методы для отображения:
    1) Главного меню
    2) Выбора режима
    3) Настройки выбранной игры
    
    Так же в игре сложения есть выбор бесконечного режима, или двадцати примеров

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,112 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool endlessMode = false;
+            int addictionScore;
+
+            Console.WriteLine("Добро пожаловать!");
+
+            ShowSettingsGameMenu();
+            addictionScore = Addition();
+
+            void ShowMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Выберите команду");
+            Console.WriteLine("1) Начать игру");
+            Console.WriteLine("1) Выход");
+        }
+            void ShowGameMenu()
+        {
+            Console.Title = "Начать игру";
+            Console.Clear();
+            Console.WriteLine("Выберите режим");
+            Console.WriteLine("1) Сложение");
+            Console.WriteLine("2) Вычитание");
+            Console.WriteLine("3) Умножение");
+            Console.WriteLine("4) Деление");
+            Console.WriteLine("5) Все вместе");
+        }
+            void ShowSettingsGameMenu()
+            {
+                int result = 0;
+                Console.Title = "Начать игру";
+                Console.Clear();
+                Console.WriteLine("Бесконечный режим ?");
+                Console.WriteLine("1) Да");
+                Console.WriteLine("2) Нет");
+
+                while (result != 1 && result != 2)
+                {
+                    result = int.Parse(Console.ReadLine());
+                    if (result == 1)
+                        endlessMode = true;
+                    else
+                        endlessMode = false;
+                }    
+        }
+            int Addition()
+            {
+                bool inGame = true;
+                int first;
+                int second;
+                Random rand = new Random();
+                int score = 0;
+
+                while (inGame)
+                {
+                    if (endlessMode)
+                    {
+                        first = rand.Next(1, 10);
+                        second = rand.Next(1, 10);
+
+                        Console.Write(first + " + " + second + " = ");
+                        int result = int.Parse(Console.ReadLine());
+
+                        if (result == (first + second))
+                        {
+                            Console.WriteLine("\tПравильный ответ!");
+                            score++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("\tНеправильный ответ!");
+                            inGame = false;
+                        }
+                    }
+                    else
+                    {
+                        int count = 0;
+                        int misCount = 0;
+                        while (count <= 19)
+                        {
+                            first = rand.Next(1, 10);
+                            second = rand.Next(1, 10);
+
+                            Console.Write(first + " + " + second + " = ");
+                            int result = int.Parse(Console.ReadLine());
+
+                            if (result == (first + second))
+                            {
+                                Console.WriteLine("\tПравильный ответ!");
+                                score++;
+                                count++;
+                            }
+                            else
+                            {
+                                Console.WriteLine("\tНеправильный ответ!");
+                                count++;
+                                misCount++;
+                            }
+                        }
+                        inGame = false;
+                        Console.WriteLine("\n" + (count - misCount) + " правильных ответов из "+ count);
+                    }
+                }
+
+                return score;
+            }
+
         }
     }
 }
